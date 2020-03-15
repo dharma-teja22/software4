970b9272a44c9127b6a646b30e49c31d2c3eef7c
dharma teja <makinateja2@gmail.com>
2020-03-16 02:20:20 +0530

    update travis - 4
    Signed-off-by: dharma teja <makinateja2@gmail.com>

--- app/src/main/java/com/example/software4/forgot1.java
+++ app/src/main/java/com/example/software4/forgot1.java
@@ -43,7 +43,7 @@ public class forgot1 extends AppCompatActivity {
                 if(c1.moveToFirst()){
                     //c2 = dtb1.rawQuery("SELECT * FROM reg WHERE username='" + s + "'", null);
                     String s=c1.getString(2);
-                    Log.d("manish",s);
+                    Log.d("manish",s);//d
                     //helo
                     Intent intent=new Intent(getApplicationContext(),forgot2.class);
                     intent.putExtra("uname",s);
