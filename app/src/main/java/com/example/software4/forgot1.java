51db6c3406539d42c07912003c910112a0212f49
dharma teja <makinateja2@gmail.com>
2020-03-15 23:48:04 +0530

    pushing

--- app/src/main/java/com/example/software4/forgot1.java
+++ app/src/main/java/com/example/software4/forgot1.java
@@ -1,11 +1,13 @@
 package com.example.software4;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,22 +39,23 @@ public class forgot1 extends AppCompatActivity {
                 if(c1.moveToFirst()){
                     //c2 = dtb1.rawQuery("SELECT * FROM reg WHERE username='" + s + "'", null);
                     String s=c1.getString(2);
-                    /*Intent intent=new Intent(getApplicationContext(),forgot1.class);
-
+                    Log.d("manish",s);
+                    Intent intent=new Intent(getApplicationContext(),forgot2.class);
                     PendingIntent pi= PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
 //Get the SmsManager instance and call the sendTextMessage method to send message
-                    SmsManager sms= SmsManager.getDefault();
-                    sms.sendTextMessage(s, null, "5567", pi,null);
+                    SmsManager sms=SmsManager.getDefault();
+                    sms.sendTextMessage(s, null, "4488", pi,null);
                     Toast.makeText(getApplicationContext(), "Message Sent successfully!",
-                            Toast.LENGTH_LONG).show();*/
+                            Toast.LENGTH_LONG).show();
+
 
 
                     /*SmsManager smsManager = SmsManager.getDefault();
                     smsManager.sendTextMessage(s, null, "Test message", null, null);
                     sendSMS(s ,"5567");*/
-                    Intent intent1=new Intent(forgot1.this,forgotpw.class);
-                    intent1.putExtra("uname",s);
-                    startActivity(intent1);
+
+                    intent.putExtra("uname",s);
+                    startActivity(intent);
                 }
                 else{
                     Toast.makeText(getApplicationContext(),"username doen't exist",Toast.LENGTH_LONG).show();
